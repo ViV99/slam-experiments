6dc3c4a128da0143e7c3de7229c220956ce761b7
m.skurikhin <m.skurikhin@tinkoff.ru>
2024-01-31 17:08:40 +0500

    feat: add initial feature matcher api

--- main.py
+++ main.py
@@ -1,29 +1,70 @@
+from typing import Sequence
 from abc import ABC, abstractmethod
 
 import cv2
 import numpy as np
+from cv2 import KeyPoint, DMatch, NORM_HAMMING
 
 
 class FeatureExtractor(ABC):
     @abstractmethod
-    def get_features(self, img: np.ndarray):
-        ...
+    def get_features(self, img: np.ndarray) -> tuple[Sequence[KeyPoint], np.ndarray]:
+        raise NotImplementedError
 
 
-class Orb2FeatureExtractor(FeatureExtractor):
+class OrbFeatureExtractor(FeatureExtractor):
     def __init__(self, n_features: int = 500) -> None:
         self.orb2 = cv2.ORB.create(nfeatures=n_features)
 
-    def get_features(self, img: np.ndarray):
+    def get_features(self, img: np.ndarray) -> tuple[Sequence[KeyPoint], np.ndarray]:
         keypoints = self.orb2.detect(img)
-        descriptors = self.orb2.compute(img, keypoints)
-        return keypoints, descriptors
+        return self.orb2.compute(img, keypoints)
+
+
+class FeatureMatcher(ABC):
+    @abstractmethod
+    def match_features(
+        self, source_descriptors: np.ndarray, query_descriptors: np.ndarray
+    ) -> Sequence:
+        raise NotImplementedError
+
+    @classmethod
+    def draw_matches(
+        cls,
+        img1: np.ndarray,
+        keypoints1: Sequence[KeyPoint],
+        img2: np.ndarray,
+        keypoints2: Sequence[KeyPoint],
+        matches: Sequence[DMatch]
+    ) -> None:
+        matches_img = cv2.drawMatches(img1, keypoints1, img2, keypoints2, matches, None)
+        cv2.imshow("Matches", matches_img)
+
+
+class BruteForceFeatureMatcher(FeatureMatcher):
+    def __init__(self, norm_type: int):
+        self.bf = cv2.BFMatcher(normType=norm_type)
+
+    def match_features(
+        self, source_descriptors: np.ndarray, query_descriptors: np.ndarray
+    ) -> Sequence[DMatch]:
+        return self.bf.match(query_descriptors, source_descriptors)
 
 
 def main():
-    orb_extractor = Orb2FeatureExtractor()
-    img = np.ndarray([])
-    print(orb_extractor.get_features(img))
+    orb_extractor = OrbFeatureExtractor(n_features=500)
+    img1 = cv2.imread("1.png")
+    img2 = cv2.imread("2.png")
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    kp1, des1 = orb_extractor.get_features(img1)
+    kp2, des2 = orb_extractor.get_features(img2)
+
+    bf_matcher = BruteForceFeatureMatcher(norm_type=NORM_HAMMING)
+    matches = bf_matcher.match_features(des1, des2)
+    bf_matcher.draw_matches(img1, kp1, img2, kp2, matches[:50])
+    cv2.waitKey()
 
 
 if __name__ == "__main__":
