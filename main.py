e5e33c75f70960255edd38f3940048fefdb3baf8
ViV99 <maximskurikhin@mail.ru>
2024-02-01 11:31:04 +0500

    feat: add distance threshold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from typing import Sequence
+from typing import Sequence, Optional
 from abc import ABC, abstractmethod
+from operator import attrgetter
 
 import cv2
 import numpy as np
@@ -43,12 +44,16 @@ class FeatureMatcher(ABC):
 
 class BruteForceFeatureMatcher(FeatureMatcher):
     def __init__(self, norm_type: int):
-        self.bf = cv2.BFMatcher(normType=norm_type)
+        self.bf = cv2.BFMatcher(normType=norm_type, crossCheck=False)
 
     def match_features(
-        self, source_descriptors: np.ndarray, query_descriptors: np.ndarray
+        self, source_descriptors: np.ndarray, query_descriptors: np.ndarray, dist_threshold: Optional[float] = None
     ) -> Sequence[DMatch]:
-        return self.bf.match(query_descriptors, source_descriptors)
+        matches = self.bf.match(query_descriptors, source_descriptors)
+        min_dist = min(matches, key=attrgetter("distance")).distance
+        if dist_threshold:
+            return [m for m in matches if m.distance < max(2 * min_dist, dist_threshold)]
+        return matches
 
 
 def main():
@@ -62,8 +67,8 @@ def main():
     kp2, des2 = orb_extractor.get_features(img2)
 
     bf_matcher = BruteForceFeatureMatcher(norm_type=NORM_HAMMING)
-    matches = bf_matcher.match_features(des1, des2)
-    bf_matcher.draw_matches(img1, kp1, img2, kp2, matches[:50])
+    matches = bf_matcher.match_features(des1, des2, 30)
+    bf_matcher.draw_matches(img1, kp1, img2, kp2, matches)
     cv2.waitKey()
 
 
