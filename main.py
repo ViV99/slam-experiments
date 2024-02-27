56391ab7a15125edbf97463ed4a54dad2fdab5da
ViV99 <maximskurikhin@mail.ru>
2024-02-27 09:02:59 +0500

    feat: pose graph for sphere example

--- main.py
+++ main.py
@@ -103,7 +103,10 @@ def main():
 
             if arr[0] == "VERTEX_SE3:QUAT":
                 idx = int(arr[1])
-                se3 = SE3(np.array(arr[2:], dtype=np.float32))
+                se3 = SE3.from_rotation_and_translation(
+                    rotation=SO3.from_quaternion_xyzw(np.array(arr[5:], dtype=np.float32)),
+                    translation=np.array(arr[2:5], dtype=np.float32)
+                )
 
                 v = g2o.VertexSE3()
                 v.set_id(idx)
@@ -116,7 +119,10 @@ def main():
 
             elif arr[0] == "EDGE_SE3:QUAT":
                 idx1, idx2 = int(arr[1]), int(arr[2])
-                se3 = SE3(np.array(arr[3:10], dtype=np.float32))
+                se3 = SE3.from_rotation_and_translation(
+                    rotation=SO3.from_quaternion_xyzw(np.array(arr[6:10], dtype=np.float32)),
+                    translation=np.array(arr[3:6], dtype=np.float32)
+                )
                 info = np.zeros((6, 6))
                 i_u, j_u = np.triu_indices(6)
                 i_l, j_l = np.tril_indices(6, k=-1)
@@ -135,10 +141,18 @@ def main():
 
     print(f"Total: {vertex_cnt} vertices | {edge_cnt} edges")
     optimizer.initialize_optimization()
-    optimizer.optimize(30)
+    optimizer.optimize(15)
 
     optimizer.save("result.g2o")
-
+    from plotly import graph_objects as go
+    fig = go.Figure()
+    fig.add_trace(go.Scatter3d(
+        x=[p.estimate().position()[0] for _, p in optimizer.vertices().items()],
+        y=[p.estimate().position()[1] for _, p in optimizer.vertices().items()],
+        z=[p.estimate().position()[2] for _, p in optimizer.vertices().items()],
+        mode="markers"
+    ))
+    fig.show()
 
 
 if __name__ == "__main__":
